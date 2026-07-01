5ad0e00c54d43209ab9d3bef66184276a4a21d47
ayoqill <aqilaiman058@gmail.com>
2026-07-01 17:59:02 +0800

    create postgres model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class FAQResponse(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    category = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
