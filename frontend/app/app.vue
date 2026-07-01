5ab2355cce37ed35d4343871d31da79d3b53d9aa
ayoqill <aqilaiman058@gmail.com>
2026-07-01 11:58:02 +0800

    add comment for routing explaination

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <NuxtRouteAnnouncer /> <!-- helps screen reader users know page changed, reacts when the route changes (at the browser url) -->
+    <NuxtPage /> <!-- shows the content of the current page -->
   </div>
 </template>
