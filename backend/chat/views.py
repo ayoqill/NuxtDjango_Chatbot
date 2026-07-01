acb25517c03e0c1354023dd4e88613f2b54afc35
ayoqill <aqilaiman058@gmail.com>
2026-07-01 18:17:47 +0800

    use posgres instead of JSON data

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,26 +1,20 @@
-import json
-from pathlib import Path
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import FAQResponse
 
 
 @api_view(["POST"])
 def chat_api(request):
     user_message = request.data.get("message", "").lower().strip()
 
-    file_path = Path(__file__).resolve().parent / "data" / "responses.json"
+    faq = FAQResponse.objects.filter(
+        question__iexact=user_message
+    ).first()
 
-    with open(file_path, "r") as file:
-        responses = json.load(file)
-
-    for item in responses:
-        question = item["question"].lower().strip()
-
-        if user_message == question:
-            return Response({
-                "reply": item["answer"]
-            })
+    if faq:
+        return Response({
+            "reply": faq.answer
+        })
 
     return Response({
         "reply": "Sorry, I don't know the answer yet."
