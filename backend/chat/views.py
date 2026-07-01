3c63e2b211ff6bb81ced805508900c06ca566fb5
ayoqill <aqilaiman058@gmail.com>
2026-07-01 13:54:01 +0800

    put JSON data and try to fetch from it

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,11 +1,27 @@
+import json
+from pathlib import Path
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
 @api_view(["POST"])
 def chat_api(request):
-    message = request.data.get("message", "")
+    user_message = request.data.get("message", "").lower().strip()
+
+    file_path = Path(__file__).resolve().parent / "data" / "responses.json"
+
+    with open(file_path, "r") as file:
+        responses = json.load(file)
+
+    for item in responses:
+        question = item["question"].lower().strip()
+
+        if user_message == question:
+            return Response({
+                "reply": item["answer"]
+            })
 
     return Response({
-        "reply": f"You said: {message}"
+        "reply": "Sorry, I don't know the answer yet."
     })
