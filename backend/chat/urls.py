b1d098cc496c176081348f4027d88c3f43c2146c
ayoqill <aqilaiman058@gmail.com>
2026-07-02 13:10:23 +0800

    Add endpoint, connect GET API to URL

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import chat_api
+from .views import chat_api, faq_list_api
 
 urlpatterns = [
     path("chat/", chat_api),
+    path("faqs/", faq_list_api),
 ]
+
+# means now we have 2 endpoints:
+# 1. /api/chat/ - for handling chat messages reply
+# 2. /api/faqs/ - for retrieving the list of FAQs
