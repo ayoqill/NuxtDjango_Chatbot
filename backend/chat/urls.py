cc783beaed32d6566d77fda434407e622bab952e
ayoqill <aqilaiman058@gmail.com>
2026-07-02 19:02:32 +0800

    connect signup API to URL

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import chat_api, faq_list_api
+from .views import chat_api, faq_list_api, register_api
 
 urlpatterns = [
     path("chat/", chat_api),
     path("faqs/", faq_list_api),
+    path("register/", register_api),
 ]
 
-# means now we have 2 endpoints:
+# means now we have 3 endpoints:
 # 1. /api/chat/ - for handling chat messages reply
 # 2. /api/faqs/ - for retrieving the list of FAQs
+# 3. /api/register/ - for registering new users
